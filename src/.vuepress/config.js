e1f928e22db97aaa2219c5e162d149c04673b9b6
丁少华 <dingshaohua@aegis-data.cn>
2023-07-25 17:30:09 +0800

    feat: 上传预览

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -3,6 +3,7 @@ import themeSidebar from "vuepress-theme-sidebar";
 
 const isDev = process.env.NODE_ENV === 'development';
 export default {
-  // base: isDev ? '/' : '/book-red-treasure/',
+  base: isDev ? '/' : '/docs/',
+  dest: 'docs',
   theme: themeSidebar(),
 };
